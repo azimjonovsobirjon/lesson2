ed2a67f36f8a5f746b852bb3697b3095afa12152
azimjonovsobirjon <azimjonovsobirjon0426@gmail.com>
2023-01-05 01:31:32 +0500

    my FullName

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Azimjonov Sobirjon Shavkatjon o'g'li
         </p>
         <a
           className="App-link"
