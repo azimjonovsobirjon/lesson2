a4248b9be3249a9968b5e11096f0c739167bb3cc
azimjonovsobirjon <azimjonovsobirjon0426@gmail.com>
2023-01-05 08:38:07 +0500

    SomeThingEnd

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Sobirjon
+          Learn React with Sobirjon !
         </a>
       </header>
     </div>
